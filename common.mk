4a07547c60f13f7cdcf4c6855170036a910e55ae
Martin J. Pollard <martin0566@gmail.com>
2022-08-04 09:55:22 -0400

    sm8350-common: Add support for Google Dialer Call recording
    [someone5678] Modularized
    
    Original commit:
    PixelExp-Project/vendor_aosp@c3b9f5c

--- common.mk
+++ common.mk
@@ -111,6 +111,10 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl-qti.recovery \
     android.hardware.boot@1.2-service
 
+# Call recording for Google Dialer
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
